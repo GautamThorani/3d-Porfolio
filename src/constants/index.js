ef00b6fedd3ddf7b18a66657be701dc2f4943240
GautamThorani <GautamThorani@users.noreply.github.com>
2025-10-17 17:48:50 +0500

    added vercel.json

--- src/constants/index.js
+++ src/constants/index.js
@@ -303,7 +303,7 @@ const projects = [
       },
     ],
     image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/GautamThorani/HINNGE",
   },
 ];
 
